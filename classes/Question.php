887f6fd921e6059e5c2fdfd8c1af161cb68ecf2a
Kristófer H <kristmundurhh@gmail.com>
2016-11-18 18:05:18 +0000

    Bæti við aðferð til að búa til nýja spurningu
    Aðferðin býr til nýja spurningu og redirect-ar notandann á nýju
    spurninguna (showthread.php)

--- classes/Question.php
+++ classes/Question.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 class Question
 {
 	private $connection;
@@ -13,21 +15,64 @@ class Question
 		}
 	}
 
-	/**
-	*	@function name: getComments
-	*
-	*	This function gets all comments in a specific question
-	*
-	* @usage example: $db_object->getComments('503');
-	*
-	* @param questionID int
-	*
-	* @returns array()
-	*/
-	public function getComments($questionID)
+    /**
+     * @function name: newQuestion
+     *
+     *    This function creates a new question, gets the ID of the new question and redirects the user to showquestion.php
+     *
+     * @usage example: $db_object->newQuestion('Need help with jQuery', 'How do i get the value of an html input textbox by ID?', '13054');
+     *
+     * @param varchar $title
+     * @param varchar $text
+     * @param int $author_id
+     * @return none
+     */
+	public function newQuestion($title,$text,$author_id)
+	{
+		$stmt = $this->connection->prepare('call NewQuestion(?,?,?)');
+		$stmt->bindParam(1,$title);
+		$stmt->bindParam(2,$text);
+		$stmt->bindParam(3,$author_id);
+
+		try {
+			// Býr til spurninguna
+			$stmt->execute();
+
+			$stmt = $this->connection->prepare('call GetLatestThreadByUserID(?)');
+			$stmt->bindParam(1,$_SESSION['uid']);
+
+			try {
+				// Sækir ID-ið á nýju spurningunni
+				$stmt->execute();
+
+				$question_id = $stmt->fetch(PDO::FETCH_ASSOC);
+				
+				// Redirect-ar notandann á nýju spurninguna
+				header("Location: showquestion.php?qid=" . $question_id);
+
+			} catch (PDOException $e) {
+				echo $e->getMessage();
+			}
+
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
+
+    /**
+     * @function name: getComments
+     *
+     *    This function gets all comments in a specific question
+     *
+     * @usage example: $db_object->getComments('503');
+     *
+     * @param int $question_id
+     * @return array
+     */
+	public function getComments($question_id)
 	{
 		$stmt = $this->connection->prepare('call GetQuestionComments(?)');
-		$stmt->bindParam(1,$questionID);
+		$stmt->bindParam(1,$question_id);
 
 		try {
 			$stmt->execute();
@@ -39,28 +84,27 @@ class Question
 		}
 	}
 
-	/**
-	*	@function name: getQuestionTags
-	*
-	*	This function gets all tags on a specific question
-	*
-	* @usage example: $db_object->getQuestionTags('253');
-	*
-	* @param questionID int
-	*
-	* @returns array()
-	*/
-	public function getQuestionTags($questionID)
+    /**
+     * @function name: getQuestionTags
+     *
+     *    This function gets all tags on a specific question
+     *
+     * @usage example: $db_object->getQuestionTags('253');
+     *
+     * @param int $question_id
+     * @return array
+     */
+	public function getQuestionTags($question_id)
 	{
 		$stmt = $this->connection->prepare('call GetQuestionTags(?)');
-		$stmt->bindParam(1,$questionID);
+		$stmt->bindParam(1,$question_id);
 
 		try {
 			$stmt->execute();
 			return $stmt->fetchAll();
 
 		} catch (PDOException $e) {
-			echo e->getMessage();
+			echo $e->getMessage();
 			return array();
 		}
 	}
