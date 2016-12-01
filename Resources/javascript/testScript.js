4f402c1bdd6cd68bc557b40985778b7b10578cf8
Daniel Thor Thorisson <danielthorr@gmail.com>
2016-12-01 16:26:46 +0000

    Added log in and sign up to index.html with popup functionality
    Added two new elements to index.html, signUp and logIn. Then I wrote a short javascript code to animate them to pup up when you click on either sign up or log in. Then they are animated to go back out of view when you click again on the sign up button, log in button or when you click the little x icon in the corner of the window

--- Resources/javascript/testScript.js
+++ Resources/javascript/testScript.js
@@ -1,7 +1,54 @@
 var main = function()
 {
+	$("#login").on("click", function(){
+		ShowUserBox("login");
+	});
+	$(".logIn").find("img").on("click", function(){
+		ShowUserBox("login");
+	});
 	
+	$("#signup").on("click", function(){
+		ShowUserBox("signup");
+	});
+	$(".signUp").find("img").on("click", function(){
+		ShowUserBox("signup");
+	});
+	
+	function ShowUserBox(clickedOn){
+	
+	if (clickedOn == "login"){
+		var $showElement = $(".logIn");
+		var topPos = "-220px";
+	}
+	else if (clickedOn == "signup"){
+		var $showElement = $(".signUp");
+		var topPos = "-270px";
+	}
+	
+	if ($showElement.hasClass("activeMsgBox")) {
+		$showElement.animate({"top" : topPos}, 160);
+		$showElement.removeClass("activeMsgBox");
+	}
+	else {
+		$showElement.animate({"top" : "120px"}, 160);
+		$showElement.addClass("activeMsgBox");
+	}
+	};
 };
 
+	/*
+var LogIn = function(){
+	
+	var $login = $(".logIn");
+	
+	if ($login.hasClass("active")) {
+		$login.animate({"top" : "-220px"}, 80);
+		$login.removeClass("active");
+	}
+	else {
+		$login.animate({"top" : "120px"}, 80);
+		$login.addClass("active");
+	}
+};*/
 
 $(document).ready(main);
